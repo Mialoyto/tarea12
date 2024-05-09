a1d6c11ddb96912d059ad2811413081f529b0b29
Miguel Loyola Torres <mialoyto1999@gmail.com>
2024-05-09 01:45:13 -0500

    Update 1:45 a.m

--- javascript/lectorqr.js
+++ javascript/lectorqr.js
@@ -1,63 +1,65 @@
-document.addEventListener("DOMContentLoaded", () => {
 
 
 
 
-    //crea elemento
-    const video = document.createElement("video");
 
-    //nuestro camvas
-    const canvasElement = document.getElementById("qr-canvas");
-    const canvas = canvasElement.getContext("2d");
+//crea elemento
+const video = document.createElement("video");
 
-    //div donde llegara nuestro canvas
-    const btnScanQR = document.getElementById("btn-scan-qr");
+//nuestro camvas
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
 
-    //lectura desactivada
-    let scanning = false;
+//div donde llegara nuestro canvas
+const btnScanQR = document.getElementById("btn-scan-qr");
 
-    //funcion para encender la camara
-    const encenderCamara = () => {
-        navigator.mediaDevices
-            .getUserMedia({ video: { facingMode: "environment" } })
-            .then(function (stream) {
-                scanning = true;
-                btnScanQR.hidden = true;
-                canvasElement.hidden = false;
-                video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-                video.srcObject = stream;
-                video.play();
-                tick();
-                scan();
-            });
-    };
+//lectura desactivada
+let scanning = false;
 
-    //funciones para levantar las funiones de encendido de la camara
-    function tick() {
-        canvasElement.height = video.videoHeight;
-        canvasElement.width = video.videoWidth;
-        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-
-        scanning && requestAnimationFrame(tick);
-    }
-
-    function scan() {
-        try {
-            qrcode.decode();
-        } catch (e) {
-            setTimeout(scan, 300);
-        }
-    }
-
-    //apagara la camara
-    const cerrarCamara = () => {
-        video.srcObject.getTracks().forEach((track) => {
-            track.stop();
+//funcion para encender la camara
+const encenderCamara = () => {
+    navigator.mediaDevices
+        .getUserMedia({ video: { facingMode: "environment" } })
+        .then(function (stream) {
+            scanning = true;
+            btnScanQR.hidden = true;
+            canvasElement.hidden = false;
+            video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+            video.srcObject = stream;
+            video.play();
+            tick();
+            scan();
         });
-        canvasElement.hidden = true;
-        btnScanQR.hidden = false;
-    };
+};
 
+//funciones para levantar las funiones de encendido de la camara
+function tick() {
+    canvasElement.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+    scanning && requestAnimationFrame(tick);
+}
+
+function scan() {
+    try {
+        qrcode.decode();
+    } catch (e) {
+        setTimeout(scan, 300);
+    }
+}
+
+//apagara la camara
+const cerrarCamara = () => {
+    video.srcObject.getTracks().forEach((track) => {
+        track.stop();
+    });
+    canvasElement.hidden = true;
+    btnScanQR.hidden = false;
+};
+
+
+document.addEventListener("DOMContentLoaded", () => {
     const activarSonido = () => {
         var audio = document.getElementById('audioScaner');
         audio.play();
@@ -97,10 +99,11 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch(`../../controllers/listsuperhero.controller.php?${params}`)
             .then((respuesta) => respuesta.json())
             .then((dato) => {
+
                 console.log(dato)
                 const tbody = document.querySelector(".table tbody");
                 const fila = document.createElement('tr');
-
+                tbody.innerHTML = ``;
                 fila.innerHTML = `
                 <td>${dato[0].superhero_name}</td>
                 <td>${dato[0].full_name}</td>
@@ -115,4 +118,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
-});
+})
